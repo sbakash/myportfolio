d610adc4e002e063faf0f35fb34fc0d31542c777
sbakash <sbakash1398@gmail.com>
2022-09-26 09:02:59 -0400

    initial changes of the portfolio
    initial changes of the portfolio which includes skills and about me

---
+++ portfolio/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header  from './components/header/Header';
+import Home  from './components/home/Home';
+import About   from './components/about/About';
+import Skills   from './components/skills/Skills';
+
+const App = () => {
+    return (
+      <><Header/>
+      <Home/>
+      <About/> 
+      <Skills/> 
+      </>
+
+
+    )
+  }
+ 
+
+export default App;
